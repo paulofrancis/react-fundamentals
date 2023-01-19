876cd5a497540375101202be78b7c4aef108dd8f
Paulo Francis <paulofrancis88@gmail.com>
2023-01-19 21:48:04 +0000

    my first component

---
+++ src/components/basic/First.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function First() {
+    const msg = 'Welcome!'
+    return (
+        <div>
+            <h2>My First Component.</h2>
+            <p>{ msg }</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import First from './components/basic/First'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const tag = <strong>Hey React!</strong>
+ReactDOM.render(
+    <div>
+        { tag }
+        <br />
+        <First></First>
+    </div>, 
+    el
+)
